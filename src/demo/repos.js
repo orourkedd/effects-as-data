fb59314b36ebd3e7af830ada676f8b959ba6d6b3
psfrankie <frankie-orourke@pluralsight.com>
2016-12-06 08:13:24 -0800

    added standard lint

--- src/demo/repos.js
+++ src/demo/repos.js
@@ -101,7 +101,7 @@ const printRepository = (list, username) => {
 
 const buildList = (repos) => {
   const l1 = map(pick(['name', 'git_url']), repos)
-  const l2 = map(({name, git_url}) => `${name}: ${git_url}`, l1)
+  const l2 = map(({name, git_url}) => `${name}: ${git_url}`, l1) // eslint-disable-line
   const l3 = l2.join('\n')
   return l3
 }
