c3103b6eaa3e0a7ccdb719ce116ff46c85018e56
psfrankie <frankie-orourke@pluralsight.com>
2016-12-06 12:52:10 -0800

    updated single line test

--- src/demo/misc.js
+++ src/demo/misc.js
@@ -1,7 +1,7 @@
 const { httpGet } = require('./actions')
 
 function * singleLine (id) {
-  return httpGet(`http://example.com/api/v1/users/${id}`)
+  yield httpGet(`http://example.com/api/v1/users/${id}`)
 }
 
 module.exports = {
