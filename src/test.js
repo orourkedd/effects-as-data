0faab053d39e12a08a0f007941ebd3d4abc2ee1a
psfrankie <frankie-orourke@pluralsight.com>
2016-12-19 20:29:59 -0800

    removed unused function

--- src/test.js
+++ src/test.js
@@ -89,43 +89,6 @@ const testIt = (fn, expected) => {
   }
 }
 
-const getNumber = (i) => {
-  switch (i + 1) {
-    case 1:
-      return 'first'
-    case 2:
-      return 'second'
-    case 3:
-      return 'third'
-    case 4:
-      return 'fourth'
-    case 5:
-      return 'fifth'
-    case 6:
-      return 'sixth'
-    case 7:
-      return 'seventh'
-    case 8:
-      return 'eighth'
-    case 9:
-      return 'ninth'
-    case 10:
-      return 'tenth'
-    case 11:
-      return 'eleventh'
-    case 12:
-      return 'twelfth'
-    case 13:
-      return 'thirteenth'
-    case 14:
-      return 'fourteenth'
-    case 15:
-      return 'fifteenth'
-    default:
-      return i + 1
-  }
-}
-
 module.exports = {
   testHandlers: curry(testHandlers),
   testFn: curry(testFn),
