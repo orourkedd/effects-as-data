212b74664a835a0a8448838d3cb29a382627af58
psfrankie <frankie-orourke@pluralsight.com>
2017-01-12 11:02:26 -0800

    try catch around run to normalize errors to reject promises

--- src/run.js
+++ src/run.js
@@ -9,13 +9,21 @@ const {
 const { handleActions } = require('./handle-actions')
 
 const run = (actionHandlers, fn, payload = null, config = {}) => {
-  const h1 = handleActions(run, actionHandlers, config)
-  return runComplete(h1, fn, payload, config).then(prop('payload'))
+  try {
+    const h1 = handleActions(run, actionHandlers, config)
+    return runComplete(h1, fn, payload, config).then(prop('payload'))
+  } catch (e) {
+    return Promise.reject(e)
+  }
 }
 
 const runComplete = (handleActions, fn, payload = null, config = {}) => {
-  let g = fn(payload)
-  return runner(handleActions, g, payload, config)
+  try {
+    let g = fn(payload)
+    return runner(handleActions, g, payload, config)
+  } catch (e) {
+    return Promise.reject(e)
+  }
 }
 
 const runTest = (actionHandlers, fn, payload = null) => {
@@ -24,20 +32,16 @@ const runTest = (actionHandlers, fn, payload = null) => {
 }
 
 const runner = (handleActions, g, input, config = {}, el) => {
-  try {
-    const el1 = getExecutionLog(el)
-    let { output, done } = nextOutput(g, input)
-    const returnResultsAsArray = Array.isArray(output)
-    const el2 = addToExecutionLog(el1, input, output)
-    if (done) return buildPayload(el2, output)
-    return handleActions(output).then((actionResults1) => {
-      handleFailedActions(actionResults1, el2, config)
-      const actionResults2 = returnResultsAsArray ? actionResults1 : unwrapArgs(actionResults1)
-      return runner(handleActions, g, actionResults2, config, el2)
-    })
-  } catch (e) {
-    return Promise.reject(e)
-  }
+  const el1 = getExecutionLog(el)
+  let { output, done } = nextOutput(g, input)
+  const returnResultsAsArray = Array.isArray(output)
+  const el2 = addToExecutionLog(el1, input, output)
+  if (done) return buildPayload(el2, output)
+  return handleActions(output).then((actionResults1) => {
+    handleFailedActions(actionResults1, el2, config)
+    const actionResults2 = returnResultsAsArray ? actionResults1 : unwrapArgs(actionResults1)
+    return runner(handleActions, g, actionResults2, config, el2)
+  })
 }
 
 const handleFailedActions = (actionResults, el, config) => {
