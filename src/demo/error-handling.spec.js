4d1ae79575681ba42f27651f295bd7d6e006f800
PS-SD-Mac1 <sandiegodev@pluralsight.com>
2017-02-09 09:37:15 -0800

    better action failure reporting

--- src/demo/error-handling.spec.js
+++ src/demo/error-handling.spec.js
@@ -1,12 +1,15 @@
-const { run, success } = require('../index')
+const { run, success, failure } = require('../index')
 const { deepEqual } = require('assert')
-const { errorToObject } = require('../util')
 
 const error = new Error('oops!')
 
 function * test () {
   yield { type: 'noop' }
   yield { type: 'test' }
+}
+
+function * testList () {
+  yield [{ type: 'noop' }, { type: 'test' }]
 }
 
 function noopHandler () {
@@ -22,7 +25,7 @@ const handlers = {
   test: testHandler
 }
 
-describe('Error Handling', () => {
+describe.only('Error Handling', () => {
   describe('onFailure', () => {
     it('should call onFailure callback with error payload', () => {
       let errorResult
@@ -40,10 +43,31 @@ describe('Error Handling', () => {
             [42, {type: 'noop'}],
             [success('noop'), {type: 'test'}]
           ],
-          errorMessage: error.message,
-          errorName: error.name,
-          errorStack: error.stack,
-          error: errorToObject(error)
+          failure: failure(error),
+          action: { type: 'test' }
+        }
+
+        deepEqual(errorResult, expected)
+      })
+    })
+
+    it('should call onFailure callback for a list of actions', () => {
+      let errorResult
+      const onFailure = (payload) => {
+        errorResult = payload
+      }
+      return run(handlers, testList, 42, {
+        name: 'myTestFunction',
+        onFailure
+      })
+      .then(() => {
+        const expected = {
+          fn: 'myTestFunction',
+          log: [
+            [42, [{type: 'noop'}, {type: 'test'}]]
+          ],
+          failure: failure(error),
+          action: { type: 'test' }
         }
 
         deepEqual(errorResult, expected)
