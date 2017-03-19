26a8bf942bc930a56d093b2c14456f6d12b1807e
psfrankie <frankie-orourke@pluralsight.com>
2017-03-19 12:44:37 -0700

    add actions to quick start functions

--- src/universal.js
+++ src/universal.js
@@ -1,10 +1,12 @@
 const { run } = require('./run')
 const handlers = require('./handlers/universal')
+const actions = require('./actions/universal')
 
 const universalRun = (fn, payload, config) => {
   return run(handlers, fn, payload, config)
 }
 
 module.exports = {
-  run: universalRun
+  run: universalRun,
+  actions
 }
