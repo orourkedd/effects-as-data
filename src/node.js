4fbd71cfa4e5a114c7fb9196884f6388cdbdde74
psfrankie <frankie-orourke@pluralsight.com>
2017-03-19 12:44:37 -0700

    add actions to quick start functions

--- src/node.js
+++ src/node.js
@@ -1,10 +1,12 @@
 const { run } = require('./run')
 const handlers = require('./handlers/node')
+const actions = require('./actions/node')
 
 const nodeRun = (fn, payload, config) => {
   return run(handlers, fn, payload, config)
 }
 
 module.exports = {
-  run: nodeRun
+  run: nodeRun,
+  actions
 }
