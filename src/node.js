723decfbf804e0303b75521f0dc514771beb2b84
psfrankie <frankie-orourke@pluralsight.com>
2017-03-19 13:06:16 -0700

    fix exports for node and universal

--- src/node.js
+++ src/node.js
@@ -1,11 +1,10 @@
 const { merge } = require('ramda')
-const { run } = require('./run')
-const all = require('./run')
+const all = require('./index')
 const handlers = require('./handlers/node')
 const actions = require('./actions/node')
 
 const nodeRun = (fn, payload, config) => {
-  return run(handlers, fn, payload, config)
+  return all.run(handlers, fn, payload, config)
 }
 
 module.exports = merge(all, {
