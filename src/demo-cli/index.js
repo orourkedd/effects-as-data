7562cba09b3968ce13349f163756bf3a12cf1a60
psfrankie <frankie-orourke@pluralsight.com>
2017-03-20 08:39:53 -0700

    update demo

--- src/demo-cli/index.js
+++ src/demo-cli/index.js
@@ -1,5 +1,5 @@
 require('babel-polyfill')
-const { runNode } = require('../node')
+const { run, handlers } = require('../node')
 const { saveRepositories } = require('./functions/save-repositories')
 
 const outputFile = 'repos.json'
@@ -7,4 +7,4 @@ const outputFile = 'repos.json'
 //  Clear terminal
 console.log('\x1Bc')
 
-runNode(saveRepositories, outputFile).catch(console.error)
+run(handlers, saveRepositories, outputFile).catch(console.error)
