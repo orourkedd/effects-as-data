3a081125751abee129ef77a3eef2d1f95fef8a95
psfrankie <frankie-orourke@pluralsight.com>
2017-07-20 09:52:51 -0700

    refactor error test util

--- src/specs/test-util.js
+++ src/specs/test-util.js
@@ -5,7 +5,9 @@ function expectError(e1, e2) {
   const ne2 = typeof e2 === 'string' ? new Error(e2) : e2
   const be1 = get(() => e2.constructor.name) ? errorToJson(ne1) : ne1
   const be2 = get(() => e1.constructor.name) ? errorToJson(ne2) : ne2
-  expect(omitStack(be1)).toEqual(omitStack(be2))
+  const oe1 = omitStack(be1)
+  const oe2 = omitStack(be2)
+  expect(oe1).toEqual(oe2)
 }
 
 function omitStack(s) {
