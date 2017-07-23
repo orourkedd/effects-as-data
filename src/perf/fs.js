f6a50dbb6b7f46a92cd6625b913a5442f4a5a038
psfrankie <frankie-orourke@pluralsight.com>
2017-07-23 13:16:30 -0700

    added per test summary

--- src/perf/fs.js
+++ src/perf/fs.js
@@ -1,47 +1,93 @@
 const { promisify } = require('util')
 const fs = require('fs')
 const readFileAsync = promisify(fs.readFile)
+const writeFileAsync = promisify(fs.writeFile)
 const { call } = require('../index')
 
-function readFileHandler({ path }) {
-  return readFileAsync(path)
+function readFileHandler({ path, options }) {
+  return readFileAsync(path, options)
 }
 
-function readFileCmd(path) {
+function readFileCmd(path, options) {
   return {
     type: 'readFile',
-    path
+    path,
+    options
   }
 }
 
-const iterations = 1000 * 20
+function writeFileHandler({ path, content, options }) {
+  return writeFileAsync(path, content, options)
+}
+
+function writeFileCmd(path, content, options) {
+  return {
+    type: 'writeFile',
+    path,
+    content,
+    options
+  }
+}
+
+function nowHandler() {
+  return Date.now()
+}
+
+function nowCmd() {
+  return {
+    type: 'now'
+  }
+}
+
+const iterations = 1000 * 10
 
 async function testStandard() {
   const filePath = '/tmp/perf.txt'
   fs.writeFileSync(filePath, 'foobar', { encoding: 'utf8' })
 
-  console.time('standard')
+  const start = Date.now()
   for (let i = 0; i < iterations; i++) {
-    await readFileAsync(filePath, { encoding: 'utf8' })
+    const now = Date.now()
+    await writeFileAsync(filePath, now.toString(), { encoding: 'utf8' })
     await readFileAsync(filePath, { encoding: 'utf8' })
   }
-
-  console.timeEnd('standard')
+  const end = Date.now()
+  return end - start
 }
 
 async function testEAD() {
   const filePath = '/tmp/perf.txt'
   fs.writeFileSync(filePath, 'foobar', { encoding: 'utf8' })
 
-  console.time('ead')
+  const start = Date.now()
   for (let i = 0; i < iterations; i++) {
-    await call({}, { readFile: readFileHandler }, function*() {
-      yield readFileCmd(filePath)
-      yield readFileCmd(filePath)
-    })
+    await call(
+      {},
+      {
+        readFile: readFileHandler,
+        writeFile: writeFileHandler,
+        now: nowHandler
+      },
+      function*() {
+        const now = yield nowCmd()
+        yield writeFileCmd(filePath, now.toString(), { encoding: 'utf8' })
+        yield readFileCmd(filePath, { encoding: 'utf8' })
+      }
+    )
   }
 
-  console.timeEnd('ead')
+  const end = Date.now()
+  return end - start
 }
 
-testStandard().then(testEAD).catch(console.log)
+async function benchmark() {
+  const standard = await testStandard()
+  const ead = await testEAD()
+  const diff = ead - standard
+  const perTransaction = diff / iterations
+  const microSecondsPerTransaction = (perTransaction * 1000).toFixed(2)
+  console.log(`Differnce for ${iterations} transactions: ${diff}ms`)
+  console.log(`Differnce per transaction: ${microSecondsPerTransaction}μs`)
+}
+
+benchmark().catch(console.error)
