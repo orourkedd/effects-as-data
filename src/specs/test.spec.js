e9f0f7616bf500aa98c01058534f0a11a69e011d
psfrankie <frankie-orourke@pluralsight.com>
2017-07-25 08:17:24 -0700

    update test framework specs

--- src/specs/test.spec.js
+++ src/specs/test.spec.js
@@ -25,37 +25,41 @@ function* yieldArray() {
 const testSingleLine = testFn(singleLine)
 const testYieldArray = testFn(yieldArray)
 
-test('testFn should pass (basic)', () => {
+test(
+  'testFn should pass (basic)',
   testFn(basic, () => {
     // prettier-ignore
     return [
       ['foo', cmds.echo('foo')],
       ['foo', 'foo']
     ]
-  })()
-})
+  })
+)
 
-test('testFn semantic should pass (basic)', () => {
+test(
+  'testFn semantic should pass (basic)',
   testFn(basic, () => {
     //  prettier-ignore
     return args('foo')
       .yieldCmd(cmds.echo('foo'))
       .yieldReturns('foo')
       .returns('foo')
-  })()
-})
+  })
+)
 
-test('testFn should curry', () => {
+test(
+  'testFn should curry',
   testFn(basic)(() => {
     // prettier-ignore
     return [
       ['foo', cmds.echo('foo')],
       ['foo', 'foo']
     ]
-  })()
-})
+  })
+)
 
-test('testFn should pass (basicMultistep)', () => {
+test(
+  'testFn should pass (basicMultistep)',
   testFn(basicMultistep, () => {
     // prettier-ignore
     return [
@@ -63,10 +67,11 @@ test('testFn should pass (basicMultistep)', () => {
       ['foo1', cmds.echo('foo2')],
       ['foo2', {s1: 'foo1', s2: 'foo2'}]
     ]
-  })()
-})
+  })
+)
 
-test('testFn semantic should pass (basicMultistep)', () => {
+test(
+  'testFn semantic should pass (basicMultistep)',
   testFn(basicMultistep, () => {
     return args('foo')
       .yieldCmd(cmds.echo('foo1'))
@@ -74,147 +79,161 @@ test('testFn semantic should pass (basicMultistep)', () => {
       .yieldCmd(cmds.echo('foo2'))
       .yieldReturns('foo2')
       .returns({ s1: 'foo1', s2: 'foo2' })
-  })()
-})
+  })
+)
 
-test('testFn should pass (basicParallel)', () => {
-  const c = [cmds.echo('foo'), cmds.echo('foo')]
+test(
+  'testFn should pass (basicParallel)',
   testFn(basicParallel, () => {
+    const c = [cmds.echo('foo'), cmds.echo('foo')]
     // prettier-ignore
     return [
       ['foo', c],
       [['foo', 'foo'], {s1: 'foo1', s2: 'foo2'}]
     ]
-  })()
-})
+  })
+)
 
-test('testFn semantic should pass (basicParallel)', () => {
-  const c = [cmds.echo('foo'), cmds.echo('foo')]
+test(
+  'testFn semantic should pass (basicParallel)',
   testFn(basicParallel, () => {
+    const c = [cmds.echo('foo'), cmds.echo('foo')]
     return args('foo')
       .yieldCmd(c)
       .yieldReturns(['foo', 'foo'])
       .returns({ s1: 'foo1', s2: 'foo2' })
-  })()
-})
+  })
+)
 
-test('testFn should pass (basicMultistepParallel)', () => {
-  const c1 = [cmds.echo('foo'), cmds.echo('foo')]
-  const c2 = [cmds.echo('foo'), cmds.echo('foo')]
+test(
+  'testFn should pass (basicMultistepParallel)',
   testFn(basicMultistepParallel, () => {
+    const c1 = [cmds.echo('foo'), cmds.echo('foo')]
+    const c2 = [cmds.echo('foo'), cmds.echo('foo')]
     // prettier-ignore
     return [
       ['foo', c1],
       [['foo', 'foo'], c2],
       [['foo', 'foo'], {s1: 'foo1', s2: 'foo2', s3: 'foo3', s4: 'foo4'}]
     ]
-  })()
-})
+  })
+)
 
-test('testFn semantic should pass (basicMultistepParallel)', () => {
-  const c1 = [cmds.echo('foo'), cmds.echo('foo')]
-  const c2 = [cmds.echo('foo'), cmds.echo('foo')]
+test(
+  'testFn semantic should pass (basicMultistepParallel)',
   testFn(basicMultistepParallel, () => {
+    const c1 = [cmds.echo('foo'), cmds.echo('foo')]
+    const c2 = [cmds.echo('foo'), cmds.echo('foo')]
     return args('foo')
       .yieldCmd(c1)
       .yieldReturns(['foo', 'foo'])
       .yieldCmd(c2)
       .yieldReturns(['foo', 'foo'])
       .returns({ s1: 'foo1', s2: 'foo2', s3: 'foo3', s4: 'foo4' })
-  })()
-})
+  })
+)
 
-test('testFn should pass (basicEmpty)', () => {
+test(
+  'testFn should pass (basicEmpty)',
   testFn(basicEmpty, () => {
     // prettier-ignore
     return [
       [null, []],
       [[], []]
     ]
-  })()
-})
+  })
+)
 
-test('testFn semantic should pass (basicEmpty)', () => {
+test(
+  'testFn semantic should pass (basicEmpty)',
   testFn(basicEmpty, () => {
     return args(null).yieldCmd([]).yieldReturns([]).returns([])
-  })()
-})
+  })
+)
 
-test('testFn should pass (eitherTestError)', () => {
+test(
+  'testFn should pass (eitherTestError)',
   testFn(eitherTestError, () => {
     // prettier-ignore
     return [
       [null, cmds.either(cmds.die('oops'), 'foo')],
       ['foo', 'foo']
     ]
-  })()
-})
+  })
+)
 
-test('testFn semantic should pass (eitherTestError)', () => {
+test(
+  'testFn semantic should pass (eitherTestError)',
   testFn(eitherTestError, () => {
     return args(null)
       .yieldCmd(cmds.either(cmds.die('oops'), 'foo'))
       .yieldReturns('foo')
       .returns('foo')
-  })()
-})
+  })
+)
 
-test('testFn should handle errors (badHandler)', () => {
+test(
+  'testFn should handle errors (badHandler)',
   testFn(badHandler, () => {
     // prettier-ignore
     return [
       [null, cmds.die('oops')],
       [new Error('oops!'), new Error('oops!')]
     ]
-  })()
-})
+  })
+)
 
-test('testFn semantic should handle errors (badHandler)', () => {
+test(
+  'testFn semantic should handle errors (badHandler)',
   testFn(badHandler, () => {
     return args(null)
       .yieldCmd(cmds.die('oops'))
       .yieldReturns(new Error('oops!'))
       .returns(new Error('oops!'))
-  })()
-})
+  })
+)
 
-test('testFn should pass (eitherTestEmpty)', () => {
+test(
+  'testFn should pass (eitherTestEmpty)',
   testFn(eitherTestEmpty, () => {
     // prettier-ignore
     return [
       [null, cmds.either(cmds.echo(null), 'foo')],
       ['foo', 'foo']
     ]
-  })()
-})
+  })
+)
 
-test('testFn semantic should pass (eitherTestEmpty)', () => {
+test(
+  'testFn semantic should pass (eitherTestEmpty)',
   testFn(eitherTestEmpty, () => {
     return args(null)
       .yieldCmd(cmds.either(cmds.echo(null), 'foo'))
       .yieldReturns('foo')
       .returns('foo')
-  })()
-})
+  })
+)
 
-test('testFn should pass (asyncTest)', () => {
+test(
+  'testFn should pass (asyncTest)',
   testFn(asyncTest, () => {
     // prettier-ignore
     return [
       [null, cmds.async({ type: 'test' })],
       [null, null]
     ]
-  })()
-})
+  })
+)
 
-test('testFn semantic should pass (asyncTest)', () => {
+test(
+  'testFn semantic should pass (asyncTest)',
   testFn(asyncTest, () => {
     return args(null)
       .yieldCmd(cmds.async({ type: 'test' }))
       .yieldReturns(null)
       .returns(null)
-  })()
-})
+  })
+)
 
 test(
   'single line should not fail',
