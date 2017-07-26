873cf29e8e2a06d55beca892b54cc7ef175d2911
psfrankie <frankie-orourke@pluralsight.com>
2017-07-26 15:05:10 -0700

    fix generator polyfill issue

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 const { isGenerator, toArray, errorToJson, toPromise } = require('./util')
 
 function call(config, handlers, fn, ...args) {
-  return run(config, handlers, fn, args)
+  if (!fn) {
+    const noFunctionMessage =
+      'A function is required. Perhaps your function is undefined?'
+    return Promise.reject(new Error(noFunctionMessage))
+  }
+  const g = fn.apply(null, args)
+  return run(config, handlers, g, args)
 }
 
 function run(config, handlers, fn, input, el, generatorOperation = 'next') {
   try {
     const el1 = getExecutionLog(el)
-    if (!fn) {
-      const noFunctionMessage =
-        'A function is required. Perhaps your function is undefined?'
-      throw new Error(noFunctionMessage)
-    }
     const { output, isList, done, fn2 } = getNextOutput(
       fn,
       input,
@@ -48,13 +49,13 @@ function unwrapResults(isList, results) {
 }
 
 function getNextOutput(fn, input, op = 'next') {
-  const g = isGenerator(fn) ? fn.apply(null, input) : fn
-  const { value: output, done } = g[op](input)
+  // const g = isGenerator(fn) ? fn.apply(null, input) : fn
+  const { value: output, done } = fn[op](input)
   return {
     output,
     isList: Array.isArray(output),
     done,
-    fn2: g
+    fn2: fn
   }
 }
 
