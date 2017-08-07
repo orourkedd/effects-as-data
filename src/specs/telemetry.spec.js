f4225ef8885b72e7dfd3f5912cc743c4de521e14
Mike Davis <michael.jacob.davis@gmail.com>
2017-08-07 08:07:07 -0700

    Add delay and timing (#18)

--- src/specs/telemetry.spec.js
+++ src/specs/telemetry.spec.js
@@ -3,68 +3,83 @@ const { handlers, functions, cmds } = require('./effects')
 const { basicMultistep, badHandler, basic } = functions
 const { sleep } = require('./test-util')
 
-test('telemetry - onCommandComplete', async () => {
-  let telemetry = []
-  const onCommandComplete = t => {
-    telemetry.push(t)
-  }
-  const config = { onCommandComplete, name: 'telemetry' }
-  const now = Date.now()
-  await call(config, handlers, basicMultistep, 'foo')
-  expect(telemetry.length).toEqual(2)
-  telemetry.forEach((t, i) => {
-    const message = 'foo' + (i + 1)
-    expect(t.success).toEqual(true)
-    expect(t.command).toEqual(cmds.echo(message))
-    expect(t.latency).toBeLessThan(5)
-    expect(t.start).toBeGreaterThanOrEqual(now)
-    expect(t.end).toBeGreaterThanOrEqual(t.start)
-    expect(t.index).toEqual(0)
-    expect(t.step).toEqual(i)
-    expect(t.result).toEqual(message)
-    expect(t.config).toEqual(config)
+test('telemetry - onCommandComplete', () => {
+  return new Promise(async resolve => {
+    let telemetry = []
+    const onCommandComplete = t => {
+      telemetry.push(t)
+    }
+    const config = { onCommandComplete, name: 'telemetry' }
+    const now = Date.now()
+    await call(config, handlers, basicMultistep, 'foo')
+    setImmediate(() => {
+      expect(telemetry.length).toEqual(2)
+      telemetry.forEach((t, i) => {
+        const message = 'foo' + (i + 1)
+        expect(t.success).toEqual(true)
+        expect(t.command).toEqual(cmds.echo(message))
+        expect(t.latency).toBeLessThan(5)
+        expect(t.start).toBeGreaterThanOrEqual(now)
+        expect(t.end).toBeGreaterThanOrEqual(t.start)
+        expect(t.index).toEqual(0)
+        expect(t.step).toEqual(i)
+        expect(t.result).toEqual(message)
+        expect(t.config).toEqual(config)
+      })
+      resolve()
+    })
   })
 })
 
-test('telemetry on error - onCommandComplete', async () => {
-  let telemetry
-  const onCommandComplete = t => {
-    telemetry = t
-  }
-  const config = { onCommandComplete }
-  const now = Date.now()
-  const message = 'oops'
-  try {
-    await call(config, handlers, badHandler, message)
-  } catch (e) {}
-  await sleep(10)
-  expect(telemetry.success).toEqual(false)
-  expect(telemetry.command).toEqual(cmds.die(message))
-  expect(telemetry.latency).toBeLessThan(5)
-  expect(telemetry.start).toBeGreaterThanOrEqual(now)
-  expect(telemetry.end).toBeGreaterThanOrEqual(telemetry.start)
-  expect(telemetry.index).toEqual(0)
-  expect(telemetry.step).toEqual(0)
-  expect(telemetry.result.message).toEqual('oops')
-  expect(telemetry.config).toEqual(config)
+test('telemetry on error - onCommandComplete', () => {
+  return new Promise(async resolve => {
+    let telemetry
+    const onCommandComplete = t => {
+      telemetry = t
+    }
+    const config = { onCommandComplete }
+    const now = Date.now()
+    const message = 'oops'
+    try {
+      await call(config, handlers, badHandler, message)
+    } catch (e) {}
+    setImmediate(() => {
+      expect(telemetry.success).toEqual(false)
+      expect(telemetry.command).toEqual(cmds.die(message))
+      expect(telemetry.latency).toBeLessThan(5)
+      expect(telemetry.start).toBeGreaterThanOrEqual(now)
+      expect(telemetry.end).toBeGreaterThanOrEqual(telemetry.start)
+      expect(telemetry.index).toEqual(0)
+      expect(telemetry.step).toEqual(0)
+      expect(telemetry.result.message).toEqual('oops')
+      expect(telemetry.config).toEqual(config)
+    })
+    resolve()
+  })
 })
 
-test('onCall', async () => {
-  let called
-  const onCall = t => {
-    called = t
-  }
-  const config = { onCall }
-  await call(config, handlers, basicMultistep, 'foo', 'bar', 'baz')
-  expect(called.args).toEqual(['foo', 'bar', 'baz'])
+test('onCall', () => {
+  return new Promise(async resolve => {
+    const onCall = called => {
+      expect(called.args).toEqual(['foo', 'bar', 'baz'])
+      resolve()
+    }
+    const config = { onCall }
+    await call(config, handlers, basicMultistep, 'foo', 'bar', 'baz')
+  })
 })
 
-test('onComplete', async () => {
-  let complete
-  const onComplete = t => {
-    complete = t
-  }
-  const config = { onComplete, name: 'telemetry' }
-  await call(config, handlers, basic, 'foo')
-  expect(complete.result).toEqual('foo')
+test('onComplete',  () => {
+  return new Promise(async resolve => {
+    const now = Date.now()
+    const onComplete = complete => {
+      expect(complete.result).toEqual('foo')
+      expect(typeof complete.latency).toEqual('number')
+      expect(complete.start).toBeGreaterThanOrEqual(now)
+      expect(complete.end).toBeGreaterThanOrEqual(complete.start)
+      resolve()
+    }
+    const config = { onComplete, name: 'telemetry' }
+    await call(config, handlers, basic, 'foo')
+  })
 })
--- src/util.js
+++ src/util.js
@@ -9,8 +9,11 @@ function toArray(a) {
 const isPromise = v => v && v.then
 const toPromise = v => (isPromise(v) ? v : Promise.resolve(v))
 
+const delay = typeof setImmediate === undefined ? fn => setTimeout(fn, 0) : setImmediate
+
 module.exports = {
   isGenerator,
   toArray,
-  toPromise
+  toPromise,
+  delay
 }
