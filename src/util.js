70107cde69355abf1250217a47b2d24f4c4d874a
Frankie O'Rourke <frankie@orourkedd.com>
2017-09-14 15:23:03 -0700

    remove comments from uuid snippet

--- src/util.js
+++ src/util.js
@@ -13,23 +13,14 @@ const delay =
   typeof setImmediate === undefined ? fn => setTimeout(fn, 0) : setImmediate;
 
 //  @see https://gist.github.com/LeverOne/1308368
-function uuid(
-  a,
-  b // placeholders
-) {
+function uuid(a, b) {
   for (
-    // loop :)
-    b = a = ""; // b - result , a - numeric variable
-    a++ < 36; //
+    b = a = "";
+    a++ < 36;
     b +=
-      (a * 51) & 52 // if "a" is not 9 or 14 or 19 or 24
-        ? //  return a random number or 4
-          (a ^ 15 // if "a" is not 15
-            ? // genetate a random number from 0 to 15
-              8 ^ (Math.random() * (a ^ 20 ? 16 : 4)) // unless "a" is 20, in which case a random number from 8 to 11
-            : 4) //  otherwise 4
-            .toString(16)
-        : "-" //  in other cases (if "a" is 9,14,19,24) insert "-"
+      (a * 51) & 52
+        ? (a ^ 15 ? 8 ^ (Math.random() * (a ^ 20 ? 16 : 4)) : 4).toString(16)
+        : "-"
   );
   return b;
 }
